20d186876bfb4e74a4ce0b534ac519df030f2790
Developerr18 <dineshnikum1@gmail.com>
2025-09-22 03:24:01 +0530

    build user sign up logic

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,9 @@ import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
+
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 5001;
 
