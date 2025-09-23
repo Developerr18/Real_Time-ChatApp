ccea6c8102cbd3665207551d3df28f8fece59f60
Developerr18 <dineshnikum1@gmail.com>
2025-09-24 02:01:39 +0530

    build send message functionality

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
@@ -15,6 +16,7 @@ export const getUsersForSidebar = async (req, res) => {
   }
 };
 
+//////////////////////////////////////////////////////
 export const getMessages = async (req, res) => {
   try {
     const { id: otherUserId } = req.params;
@@ -30,6 +32,34 @@ export const getMessages = async (req, res) => {
     res.status(200).json(messages);
   } catch (err) {
     console.error("Error in getMessages:", err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+//////////////////////////////////////////////////////
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+    res.status(201).json(newMessage);
+  } catch (err) {
+    console.error("Error in sendMessage:", err);
     res.status(500).json({ message: "Server error" });
   }
 };
