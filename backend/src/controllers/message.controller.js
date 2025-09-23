f84ff5ef406f457e7fc01cfadec472a4fcbeb550
Developerr18 <dineshnikum1@gmail.com>
2025-09-24 01:40:42 +0530

    build functionality for get messages between two users

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,35 @@
+import Message from "../models/message.model.js";
+import User from "../models/user.model.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+
+    res.status(200).json(filteredUsers);
+  } catch (err) {
+    console.error("Error in getUsersForSidebar:", err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: otherUserId } = req.params;
+    const loggedInUserId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: loggedInUserId, receiverId: otherUserId },
+        { senderId: otherUserId, receiverId: loggedInUserId },
+      ],
+    });
+
+    res.status(200).json(messages);
+  } catch (err) {
+    console.error("Error in getMessages:", err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
